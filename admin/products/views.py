ee7dac1399bed22de7cc8307acd228d61a7010a4
mehedi-mim <mehedi.mim.bd@gmail.com>
2022-01-02 19:08:41 +0600

    products table added

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+from rest_framework.response import Response
+
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
